b0b47bdb6beb1e89548f0ee3f542259e2fdb1676
Emilio Layrana <emiliolayrana@gmail.com>
2018-10-02 13:01:22 +0200

    clases para configuracion de entorno de produccion y desarrallo añadidas

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config():
+    SECRET_KEY = os.getenv('SECRET_KEY','Prueba123')
+    SQLALCHEMY_TYRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,
+                                                          'data-dev.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,
+                                                          'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
