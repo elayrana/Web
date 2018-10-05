6aa1b75614271b5c062830f66f82294ef13456da
Emilio Layrana <emiliolayrana@gmail.com>
2018-10-05 09:48:39 +0200

    añadidos warnings en los procesos de autenticacion

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,21 +3,19 @@ from . import auth
 from .forms import SignUpForm, LogInForm
 from ..models import Users
 from .. import db
-from flask_login import login_user, logout_user,login_required
+from flask_login import login_user, logout_user, login_required
 
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignUpForm()
     if form.validate_on_submit():
-        kwargs = {"username": form.username,
-                  "email": form.email,
-                  "password": form.password}
         user = Users(username=form.username.data,
                      email=form.email.data,
                      password=form.password.data)
         db.session.add(user)
         db.session.commit()
+        flash('Yocan now log In', 'information')
         return redirect(url_for('auth.login'))
     return render_template('auth/signup.html', form=form)
 
@@ -31,7 +29,7 @@ def login():
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me)
             return redirect(url_for('main.index'))
-        flash('User or password incorrect')
+        flash('User or password incorrect', 'error')
     return render_template('/auth/login.html', form=form)
 
 
@@ -39,5 +37,5 @@ def login():
 @login_required
 def logout():
     logout_user()
-    flash('You have been log out')
+    flash('You have been log out', 'information')
     return redirect(url_for('main.index'))
