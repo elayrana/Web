986b5e495e10fdd0a909cc9347a8696d76db6039
Emilio Layrana <emiliolayrana@gmail.com>
2018-10-05 15:15:38 +0200

    Creando tokens verificacion por email de registro nuevo usuario

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask import render_template
-from . import main 
+from . import main
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
+
     return render_template('index.html')
-    
--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
+from flask import current_app
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from .import login_manager
+from itsdangerous import JSONWebSignatureSerializer as Serializer
 
 
 class Users(UserMixin, db.Model):
@@ -10,6 +12,23 @@ class Users(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String, unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     @property
     def password(self):
