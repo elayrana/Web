5715a4abaae4c6ea98513a45374940576f1f344b
Emilio <emiliolayrana@gmail.com>
2018-10-30 16:48:02 +0100

    Añadiendo Roles

--- app/models.py
+++ app/models.py
@@ -1,17 +1,54 @@
 from flask import current_app
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from .import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
+
+class Permissions:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMENTS = 0x08
+    ADMINISTER = 0x80
+
+
 class Roles(db.Model):
     __tablename__ = 'roles'
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     default = db.Column(db.Boolean, default=False, index=True)
     permissions = db.Column(db.Integer)
-    
+    users = db.relationship('Users', backref='roles', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permissions.FOLLOW |
+                     Permissions.COMMENT |
+                     Permissions.WRITE_ARTICLES, True),
+            'Moderator': (Permissions.FOLLOW |
+                          Permissions.COMMENT |
+                          Permissions.WRITE_ARTICLES |
+                          Permissions.MODERATE_COMENTS, False),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Roles.query.filter_by(name=r).first()
+            if role is None:
+                role = Roles(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
+
+    def __repr__(self):
+        return '\nID:{} Name:{} Permissions:{:08b}'.format(self.id,
+                                                           self.name,
+                                                           self.permissions)
+
+
 class Users(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -20,6 +57,14 @@ class Users(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Roles.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Roles.query.filter_by(default=True).first()
 
     def generate_confirmation_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
@@ -52,7 +97,25 @@ class Users(UserMixin, db.Model):
         else:
             return check_password_hash(self.password_hash, password)
 
+    def can(self, permissions):
+        return self.role is not None and \
+            self.role.permissions == permissions
+
+    def is_administrator(self):
+        return self.can(Permissions.ADMINISTER)
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator():
+        return False
+
 
 @login_manager.user_loader
 def user_load(user_id):
     return Users.query.get(int(user_id))
+
+
+login_manager.anonymous_user = AnonymousUser
