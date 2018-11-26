3136abf2898348a0e579e2686eb589476a11b616
Emilio <emiliolayrana@gmail.com>
2018-11-26 09:24:30 +0100

    Importando roles a la shell

--- flasky.py
+++ flasky.py
@@ -1,7 +1,7 @@
 from app import create_app
 from app import db
 from flask_migrate import Migrate
-from app.models import Users
+from app.models import Users, Roles
 
 
 app = create_app('default')
@@ -10,4 +10,4 @@ mograte = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, Users=Users)
+    return dict(app=app, db=db, Users=Users, Roles=Roles)
